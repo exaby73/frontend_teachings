d9247a22b04905a65a2735d20409abff730529e5
exaby73 <nabeelparkar99@gmail.com>
2024-03-10 10:50:19 +0530

    Changes

--- main.js
+++ main.js
@@ -1,75 +1,64 @@
-const firstNumberInput = document.getElementById("first-number");
-const secondNumberInput = document.getElementById("second-number");
+// @ts-nocheck
 
-const addButton = document.getElementById("add-button");
-const subButton = document.getElementById("sub-button");
-const mulButton = document.getElementById("mul-button");
-const divButton = document.getElementById("div-button");
+const input = document.querySelector(".input input");
+const addButton = document.querySelector(".input button");
+const list = document.querySelector(".list");
 
-const answer = document.getElementById("answer");
+const data = [];
 
-if (
-  !firstNumberInput ||
-  !secondNumberInput ||
-  !addButton ||
-  !subButton ||
-  !mulButton ||
-  !divButton ||
-  !answer
-) {
-  throw new Error("One or more elements not found");
+function saveTodos() {
+  const json = JSON.stringify(data);
+  localStorage.setItem("data", json);
 }
 
-addButton.addEventListener("click", () => {
-  // @ts-ignore
-  const fn = parseInt(firstNumberInput.value);
-  // @ts-ignore
-  const sn = parseInt(secondNumberInput.value);
+function removeTodo(id) {
+  const index = data.findIndex((v) => v.id === id);
+  if (index === -1) return;
+  console.log(data);
+  data.splice(index, 1);
+  console.log(data);
+  saveTodos();
+}
 
-  if (isNaN(fn) || isNaN(sn)) {
-    answer.innerText = "Please input valid numbers";
-  } else {
-    answer.innerText = (fn + sn).toString();
+function addTodo(value) {
+  const todo = {
+    id: data.length ? data[data.length - 1].id + 1 : 1,
+    title: value,
+  };
+
+  data.push(todo);
+  saveTodos();
+
+  const addInput = document.createElement("input");
+  addInput.setAttribute("type", "checkbox");
+  addInput.addEventListener("click", () => {
+    removeTodo(todo.id);
+    document.querySelector('li[data-id="' + todo.id + '"]').remove();
+  });
+
+  const p = document.createElement("p");
+  p.innerText = value;
+
+  const li = document.createElement("li");
+  li.append(addInput, " ", p);
+  li.setAttribute("data-id", todo.id);
+
+  list.append(li);
+  input.value = "";
+}
+
+addButton.addEventListener("click", () => addTodo(input.value));
+
+input.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    addTodo(e.target.value);
   }
 });
 
-subButton.addEventListener("click", () => {
-  // @ts-ignore
-  const fn = parseInt(firstNumberInput.value);
-  // @ts-ignore
-  const sn = parseInt(secondNumberInput.value);
-
-  if (isNaN(fn) || isNaN(sn)) {
-    answer.innerText = "Please input valid numbers";
-  } else {
-    answer.innerText = (fn - sn).toString();
+const dataFromLS = localStorage.getItem("data");
+if (dataFromLS) {
+  const todos = JSON.parse(dataFromLS);
+  for (const todo of todos) {
+    addTodo(todo.title);
   }
-});
-
-mulButton.addEventListener("click", () => {
-  // @ts-ignore
-  const fn = parseInt(firstNumberInput.value);
-  // @ts-ignore
-  const sn = parseInt(secondNumberInput.value);
-
-  if (isNaN(fn) || isNaN(sn)) {
-    answer.innerText = "Please input valid numbers";
-  } else {
-    answer.innerText = (fn * sn).toString();
-  }
-});
-
-divButton.addEventListener("click", () => {
-  // @ts-ignore
-  const fn = parseInt(firstNumberInput.value);
-  // @ts-ignore
-  const sn = parseInt(secondNumberInput.value);
-
-  if (isNaN(fn) || isNaN(sn)) {
-    answer.innerText = "Please input valid numbers";
-  } else if (sn === 0) {
-    answer.innerText = "Cannot divide by zero";
-  } else {
-    answer.innerText = (fn / sn).toString();
-  }
-});
+}
